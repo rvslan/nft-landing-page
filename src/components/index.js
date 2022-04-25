590c9d531f5d820927ac2e8ae6508a22ebb23fc8
Ruslan <rvslan@pm.me>
2022-04-25 12:21:42 +0200

    Better handled components export

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+export { default as Card } from './Card';
+export { default as Clients } from './Clients';
+export { default as Footer } from './Footer';
+export { default as Home } from './Home';
+export { default as Like } from './Like';
+export { default as Navbar } from './Navbar';
+export { default as Release } from './Release';
+export { default as Signup } from './Signup';
+export { default as SuperRare } from './SuperRare';
+export { default as ScrollToTop } from './ScrollToTop';
+export { default as Free } from './Free';
